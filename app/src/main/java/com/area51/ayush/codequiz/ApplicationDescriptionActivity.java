cfd7dbd96ae311e6a35942494b0ee0213496dc72
Ayush Soni <ayushsoni.in@gmail.com>
2016-10-19 01:06:20 +0530

    SplashScreenActivity, ApplicationDescriptionActivity fully functional. LoginActivity initiated.

--- app/src/main/java/com/area51/ayush/codequiz/ApplicationDescriptionActivity.java
+++ app/src/main/java/com/area51/ayush/codequiz/ApplicationDescriptionActivity.java
@@ -1,13 +1,24 @@
 package com.area51.ayush.codequiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ApplicationDescriptionActivity extends AppCompatActivity {
-
+    Button continueButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application_description);
+        continueButton = (Button) findViewById(R.id.continueButton);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/area51/ayush/codequiz/SplashScreenActivity.java
+++ app/src/main/java/com/area51/ayush/codequiz/SplashScreenActivity.java
@@ -1,13 +1,31 @@
 package com.area51.ayush.codequiz;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
+        /** Duration of wait **/
+        private final int SPLASH_DISPLAY_LENGTH = 3000;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        /** Called when the activity is first created. */
+        @Override
+        public void onCreate(Bundle icicle) {
+            super.onCreate(icicle);
+            setContentView(R.layout.activity_splash_screen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+            new Handler().postDelayed(new Runnable(){
+                @Override
+                public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                    Intent mainIntent = new Intent(SplashScreenActivity.this,ApplicationDescriptionActivity.class);
+                    SplashScreenActivity.this.startActivity(mainIntent);
+                    SplashScreenActivity.this.finish();
+                }
+            }, SPLASH_DISPLAY_LENGTH);
+        }
     }
-}
