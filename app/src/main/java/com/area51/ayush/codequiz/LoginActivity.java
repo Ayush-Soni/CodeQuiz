2fc36c4b04c2b5ba094d52e8dd7b41c7fe300c24
Ayush Soni <ayushsoni.in@gmail.com>
2016-10-19 02:41:50 +0530

    LoginActivity partially functional. Needs sqlite integration.

--- app/src/main/java/com/area51/ayush/codequiz/LoginActivity.java
+++ app/src/main/java/com/area51/ayush/codequiz/LoginActivity.java
@@ -2,12 +2,23 @@ package com.area51.ayush.codequiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Get username
+                //Get password
+                //Authenticate and start activity displaying topics
+            }
+        });
     }
 }
